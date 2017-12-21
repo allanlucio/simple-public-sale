a8c8cde825490cf2c836b89a4491ff16d87a8f0f
allanufal <allan-lucio@hotmail.com>
2017-12-21 15:32:24 -0300

    resolved #16

--- simple_public_sale/core/admin.py
+++ simple_public_sale/core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Evento)
+admin.site.register(Prenda)
+admin.site.register(Movimento)
+admin.site.register(Doador)
+admin.site.register(TipoPrenda)
+admin.site.register(Arrematador)
 
