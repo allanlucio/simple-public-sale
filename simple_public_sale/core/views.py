9aadae69fc06c910a122199958b9cf5c50481d76
joaopauloufal <35301882paulo>
2018-01-02 23:49:27 -0300

    Validation of closing values.

--- simple_public_sale/core/views.py
+++ simple_public_sale/core/views.py
@@ -1,6 +1,8 @@
 from channels import Channel
+from decimal import Decimal
 from django.core import serializers
 from django.core.cache import cache
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 import json
 # Create your views here.
@@ -41,9 +43,22 @@ def manage_event(request,evento_id):
         arrematador=Arrematador.objects.get_or_create(nome_arrematador=arrematador_nome)[0]
 
         movimento=Movimento(arrematador_fk=arrematador,prenda_fk=prenda,valor_arremate=valor)
-        movimento.save()
 
-        send_to_evento(evento=evento)
+
+        movimento_anterior = Movimento.objects.filter(prenda_fk = prenda_id)[0]
+
+        if movimento_anterior != None:
+
+
+            if Decimal(movimento.valor_arremate) <= movimento_anterior.valor_arremate:
+                raise ValidationError("Valor do Arremate menor do que o valor atual!")
+            else:
+                movimento.save()
+                send_to_evento(evento=evento)
+        else:
+            if Decimal(movimento.valor_arremate) <= prenda.inicial:
+                raise ValidationError("Valor do Arremate menor do que o valor atual!")
+
         # data=get_data_stream_view(evento)
 
         # a=Channel('send-to-group').send({'message': data},immediately=True)
